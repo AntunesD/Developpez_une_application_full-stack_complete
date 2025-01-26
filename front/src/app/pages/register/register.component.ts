f4e2d0669da7563bd82562a37f97294fc2b8f897
AntunesD <antunesd90@gmail.com>
2025-01-26 21:59:50 +0100

    Vérifie la validité du mot de passe en front

--- front/src/app/pages/register/register.component.ts
+++ front/src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,9 +7,11 @@ import { Component, HostListener } from '@angular/core';
 })
 export class RegisterComponent {
   isDesktop: boolean = false;
+  registerForm!: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
     this.checkScreenSize();
+    this.createForm();
   }
 
   @HostListener('window:resize', ['$event'])
@@ -19,4 +22,53 @@ export class RegisterComponent {
   checkScreenSize() {
     this.isDesktop = window.innerWidth >= 768; // Ajustez la largeur selon vos besoins
   }
-} 
+
+  createForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, this.passwordValidator]]
+    });
+  }
+
+  // Validation personnalisée pour le mot de passe
+  passwordValidator(control: any) {
+    const password = control.value;
+    const minLength = 8;
+    const regex = {
+      number: /[0-9]/,
+      lowercase: /[a-z]/,
+      uppercase: /[A-Z]/,
+      special: /[!@#$%^&*(),.?":{}|<>]/
+    };
+
+    if (!password) return null;
+
+    if (password.length < minLength) {
+      return { minLength: true };
+    }
+    if (!regex.number.test(password)) {
+      return { number: true };
+    }
+    if (!regex.lowercase.test(password)) {
+      return { lowercase: true };
+    }
+    if (!regex.uppercase.test(password)) {
+      return { uppercase: true };
+    }
+    if (!regex.special.test(password)) {
+      return { special: true };
+    }
+
+    return null;
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      // Traitement du formulaire
+      console.log('Form submitted', this.registerForm.value);
+    } else {
+      console.log('Form invalid');
+    }
+  }
+}
